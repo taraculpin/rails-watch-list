c153612fef7fff9d604b8d5f5e5aaaa60a5c2e1f
Tara Culpin <taraculpin@gmail.com>
2022-02-18 13:26:27 +0000

    changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index new create show] do
+  resources :lists, only: %i[index new create show], path: '' do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
